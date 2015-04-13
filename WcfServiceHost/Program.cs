759ccf18769d63a283f2c50871195f60f19a86a7
Artem <trofimov9artem@gmail.com>
2015-04-13 13:06:28 +0400

    Initial commit. Added wcf service & host.

---
+++ WcfServiceHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Web;
+using WcfServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace WcfServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(TestService));
+            try
+            {
+                host.Open();
+                Console.WriteLine("Press <ENTER> to terminate");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (CommunicationException cex)
+            {
+                Console.WriteLine("An exception occurred: {0}", cex.Message);
+                host.Abort();
+            }
+        }
+    }
+}

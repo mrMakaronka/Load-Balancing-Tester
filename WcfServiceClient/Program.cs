938675cea14cd6ab120e022639d1640f0c0f2f2c
Artem <trofimov9artem@gmail.com>
2015-04-13 13:08:11 +0400

    Added client for test wcf service.

---
+++ WcfServiceClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var testService = new TestServiceClient())
+                {
+                    string message = testService.GetData(11);
+                    Console.WriteLine(message);
+                }
+            }
+            catch (CommunicationException cex)
+            {
+                Console.WriteLine("An exception occurred: {0}", cex.Message);
+            }
+        }
+    }
+}
